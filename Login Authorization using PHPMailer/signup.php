6505870ab16f520a2683a76f7d7e36b7f15a7f81
Huzaifa Ahmed <116560901+huzaifa-developer@users.noreply.github.com>
2024-12-21 10:25:20 +0500

    Add files via upload

---
+++ Login Authorization using PHPMailer/signup.php
@@ -0,0 +1,104 @@
+<?php
+include "db.php";
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+include "PHPMailer/Exception.php";
+include "PHPMailer/PHPMailer.php";
+include "PHPMailer/SMTP.php";
+
+
+if(isset($_POST["signup"])){
+  $uname=$_POST["uname"];
+  $uemail=$_POST["uemail"];
+  $upassword=$_POST["upassword"];
+  $ucontact=$_POST["ucontact"];
+  $uage=$_POST["uage"];
+  $uaddress=$_POST["uaddress"];
+  $otp=rand(100000,999999);
+  $sql="INSERT INTO `user`(`email`, `password`, `contact`, `age`, `address`, `otp`, `username`) VALUES ('$uemail','$upassword','$ucontact','$uage','$uaddress','$otp','$uname')";
+  $result=mysqli_query($conn,$sql);
+
+
+if($result){
+  $mail = new PHPMailer(true);
+  try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'huzaifaahmedpk1@gmail.com';                     //SMTP username
+    $mail->Password   = 'ohtf ijwx wcqu uybg';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('huzaifaahmedpk1@gmail.com', 'Mailer');
+    $mail->addAddress($uemail, 'Joe User');     //Add a recipient
+
+    //Attachments
+   
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Sign up OTP';
+    $mail->Body    = 'This is your OTP'.$otp;
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+}
+}
+?>
+<!DOCTYPE html>
+
+
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<title>Practice
+</title>
+</head>
+<body>
+
+<form method="post">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">user Name</label>
+    <input type="text" name="uname" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">email</label>
+    <input type="text" name="uemail" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" name="upassword" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">contact</label>
+    <input type="number" name="ucontact" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">age</label>
+    <input type="number" name="uage" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">address</label>
+    <input type="text" name="uaddress" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <button type="submit" name="signup" class="btn btn-primary">signup</button>
+</form>
+
+</body>
+</html>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/js/bootstrap.min.js"></script>
